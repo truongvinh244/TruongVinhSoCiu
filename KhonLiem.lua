286ff92f037d4fb77353af487e2a681f823a3861
Truong Vinh <congvinhtruong100@gmail.com>
2024-12-20 20:45:27 +0700

    Add files via upload

---
+++ KhonLiem.lua
@@ -0,0 +1,4 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+script.Parent.Text = "Đụ Má Mày Khôn Liềm"
+getgenv().Key = "veXAeRKBPTwkdcjuCayFKKJbKpMLwSYT" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))() 
