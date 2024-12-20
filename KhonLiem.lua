f02042d558d88f96d443aef3bd52c6b3cc1cd6a1
Truong Vinh <congvinhtruong100@gmail.com>
2024-12-20 21:00:26 +0700

    Add files via upload

--- KhonLiem.lua
+++ KhonLiem.lua
@@ -1,4 +1,11 @@
+local screenSize = game.Workspace.CurrentCamera.ViewportSize
+local textLabel = script.Parent
+textLabel.Position = UDim2.new(0.5, 0, 0.5, 0)
+textLabel.Text = "Đụ Má Mày"
+wait(10)
+textLabel.Visible = false
+
+
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
-script.Parent.Text = "Đụ Má Mày Khôn Liềm"
 getgenv().Key = "veXAeRKBPTwkdcjuCayFKKJbKpMLwSYT" 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))() 
