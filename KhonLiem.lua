ee3e30bf004622c1cfcbac09ce6c436d27ff3a5f
Truong Vinh <congvinhtruong100@gmail.com>
2024-12-23 20:37:51 +0700

    Add files via upload

--- KhonLiem.lua
+++ KhonLiem.lua
@@ -1,3 +1,1 @@
-getgenv().Key = "MARU-INRIS-01SFN-XSGF-BU8AO-H9W0"
-getgenv().id = "988398183799455764"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+print("hellowwo4le")
