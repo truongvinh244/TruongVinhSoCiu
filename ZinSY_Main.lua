7cc9e845ce8b75a252299c5b93d319b661e625f3
Truong Vinh <congvinhtruong100@gmail.com>
2025-06-15 18:12:32 +0700

    Add files via upload

---
+++ ZinSY_Main.lua
@@ -0,0 +1,8 @@
+print("ZinSY Hub - by truonvinh244")
+if game.PlaceId == 3101667897 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/truongvinh244/ZinSY_Roblox/refs/heads/main/legend_of_speed.lua"))()
+else
+    print("Game < ", game.Name, " > Không được hỗ trợ trong ZinSY Hub")
+end
+
+-- loadstring(game:HttpGet(""))()
