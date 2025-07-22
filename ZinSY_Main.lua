f074135268a19450e9e69a6b79b0cbf0360d998a
Truong Vinh <congvinhtruong100@gmail.com>
2025-07-22 20:27:32 +0700

    Add files via upload

--- ZinSY_Main.lua
+++ ZinSY_Main.lua
@@ -1,12 +1,9 @@
--- ZinSY_Roblox By TruongVinhSoCiu
 local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
--- ZinSY Hub - Legend of Speed
+
+-- Legend of Speed
 if game.PlaceId == 3101667897 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/truongvinh244/ZinSY_Roblox/refs/heads/main/legend_of_speed.lua"))()
-else
-    print("Game < ", game.Name, " > Không được hỗ trợ trong ZinSY Hub")
+-- Grow A Garden
+elseif game.PlaceId == 126884695634066 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/truongvinh244/ZinSY_Roblox/refs/heads/main/grow_a_garden.lua"))()
 end
-
--- loadstring(game:HttpGet(""))()
-
--- loadstring(game:HttpGet("https://github.com/truongvinh244/TruongVinhSoCiu/blob/main/ZinSY_Main.lua"))()
