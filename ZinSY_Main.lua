2f4571f136a7c6520cb769a3d6bb10d609cf3ea8
Truong Vinh <congvinhtruong100@gmail.com>
2025-08-12 19:04:41 +0700

    Update ZinSY_Main.lua

--- ZinSY_Main.lua
+++ ZinSY_Main.lua
@@ -8,11 +8,11 @@ function closeui()
     ScreenGui.Parent = player:WaitForChild("PlayerGui")
     ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
     ImageButton.Parent = ScreenGui
-    ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+    ImageButton.BackgroundColor3 = Color3.fromRGB(88, 193, 246)
     ImageButton.BorderSizePixel = 0
     ImageButton.Position = UDim2.new(0.0304, 0, 0.128, 0)
     ImageButton.Size = UDim2.new(0, 40, 0, 40)
-    ImageButton.Image = "rbxassetid://96036361413632"
+    ImageButton.Image = "rbxassetid://180DAY-ae9449f0beb60bc21ec99719d222ddb0"
     ImageButton.Draggable = true
     ImageButton.Active = true
     ImageButton.Selectable = true
@@ -32,13 +32,13 @@ else
     closeui()
 end
 
--- Legend of Speed
-if game.PlaceId == 3101667897 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/truongvinh244/ZinSY_Roblox/refs/heads/main/legend_of_speed.lua"))()
--- Grow A Garden
-elseif game.PlaceId == 126884695634066 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/truongvinh244/ZinSY_Roblox/refs/heads/main/grow_a_garden.lua"))()
-end
-
+if game.PlaceId == 2753915549 then
+    print("ZinSY Hub - Loading Blox Fruit Sea 1")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/truongvinh244/ZinSY_Roblox/refs/heads/main/bfsea1.lua"))()
+elseif game.placeId == 	4442272183 then
+    print("ZinSY Hub - Loading Blox Fruit Sea 2")
+elseif game.PlaceId == 7449423635 then
+    print("ZinSY Hub - Loading Blox Fruit Sea 3")
+end 
 
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/truongvinh244/TruongVinhSoCiu/refs/heads/main/ZinSY_Main.lua"))()
